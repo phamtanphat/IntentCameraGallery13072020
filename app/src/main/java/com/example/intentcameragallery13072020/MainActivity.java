84422ca27ac3c2d6a908cfead42d2504c6e9abb5
phatdroid94 <phatdroid94@gmail.com>
2020-09-04 19:21:35 +0700

    open camera

--- app/src/main/java/com/example/intentcameragallery13072020/MainActivity.java
+++ app/src/main/java/com/example/intentcameragallery13072020/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.intentcameragallery13072020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.security.Permission;
+import java.security.Permissions;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnCamera, mBtnGallery;
+    ImageView mImg;
+    int REQUEST_CODE_CAMERA = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCamera = findViewById(R.id.buttonCamera);
+        mBtnGallery = findViewById(R.id.buttonGallery);
+        mImg = findViewById(R.id.imageview);
+
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.
+                        checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_CAMERA
+                    );
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CODE_CAMERA);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
